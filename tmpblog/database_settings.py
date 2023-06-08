f9de088b8a5db4e876f2cbba2ca65b9431d57ada
khjop0502 <khjop0502@gmail.com>
2023-06-08 13:59:29 +0900

    first commit

---
+++ tmpblog/database_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "tmpblogdb",  
+        "USER": "tmpblogdbadmin",  
+        "PASSWORD": "Temp1234!",  
+        "HOST": "localhost",  
+        "PORT": "3306",  
+    }
+}
